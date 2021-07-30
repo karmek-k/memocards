8082b5bdfb8d659b5c2598bf3aca684cb67a3b90
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 10:13:16 +0200

    Init express app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,6 @@
-console.log('hello, world');
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
