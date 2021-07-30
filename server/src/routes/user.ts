85bb0f19500418c6f4c8f311741765bfef57c367
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 11:12:26 +0200

    Add token route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,18 +1,41 @@
-import { hash } from 'argon2';
+import argon2 from 'argon2';
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
 import { User } from '../models/User';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
 router.post('/', async (req, res) => {
   const user = new User();
   user.username = req.body.username;
-  user.password = await hash(req.body.password);
+  user.password = await argon2.hash(req.body.password);
 
   await getRepository(User).save(user);
 
   return res.status(201).send({ username: user.username });
+});
+
+router.post('/token', async (req, res) => {
+  const user = await getRepository(User).findOne({
+    where: { username: req.body.username }
+  });
+
+  if (!user) {
+    return res.status(404).send();
+  }
+
+  const secret: string = process.env.JWT_SECRET!;
+
+  if (!(await argon2.verify(user.password, req.body.password))) {
+    return res.status(401).send();
+  }
+
+  const token = jwt.sign({ username: user.username }, secret, {
+    expiresIn: '1d'
+  });
+
+  return res.send({ token });
 });
 
 export default router;
