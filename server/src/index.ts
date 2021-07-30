15f2b5126aa75218026a77b136df0e344132746a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 13:35:09 +0200

    Use jwt strategy in passport

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import 'reflect-metadata';
 import express from 'express';
 import { createConnection } from 'typeorm';
 import dbConfig from './config/db';
-import dotenv from 'dotenv';
-dotenv.config();
-
+import passport from 'passport';
+import jwtStrategy from './config/jwtStrategy';
 import UserRouter from './routes/user';
 
 const app = express();
@@ -13,6 +15,7 @@ const port = process.env.PORT || 8000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+passport.use(jwtStrategy);
 
 // Routes
 app.use('/user', UserRouter);
