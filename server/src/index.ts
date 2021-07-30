e423ab4cb1df43ca0475343ef6d3c504b20403aa
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 10:23:42 +0200

    Add typeorm to the project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,15 @@
+import 'reflect-metadata';
 import express from 'express';
+import { createConnection } from 'typeorm';
+import dbConfig from './config/db';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+createConnection(dbConfig)
+  .then(() => {
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  })
+  .catch(console.error);
