c88927c1dd07a373a8a4f8fc3f33a9dc3cfb2750
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 13:49:08 +0200

    Set next type to NextFunction

--- server/src/validation/middleware.ts
+++ server/src/validation/middleware.ts
@@ -1,9 +1,10 @@
+import { NextFunction } from 'express';
 import { ValidationChain, validationResult } from 'express-validator';
 
 function validate(validator: ValidationChain[]): any {
   return [
     validator,
-    (req: any, res: any, next: any) => {
+    (req: any, res: any, next: NextFunction) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).send();
