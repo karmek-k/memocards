851d60da3c2c5bf0341f5846475918cd822e01b0
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 10:49:44 +0200

    Add body parser middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 createConnection(dbConfig)
   .then(() => {
     app.listen(port, () => console.log(`Listening on port ${port}`));
