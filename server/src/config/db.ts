7063d7eaae7becd14a2b39597dd000476562ffcc
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-30 10:31:44 +0200

    Add synchronize: true to ORM options

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,7 +3,8 @@ import { ConnectionOptions } from 'typeorm';
 const config: ConnectionOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: []
+  entities: [],
+  synchronize: true
 };
 
 export default config;
