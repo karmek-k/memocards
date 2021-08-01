34b64a92ccc91a21417d93df4f0fa4794cc59606
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-01 17:41:22 +0200

    Add me tests

---
+++ server/tests/user/me.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import app from '../../src/server';
+import { afterEachHandler, beforeEachHandler } from '../functions';
+
+describe('User creation tests', () => {
+  let jwt: string;
+  const creds = {
+    username: 'tester',
+    password: 'secret'
+  };
+
+  beforeEach(async () => {
+    await beforeEachHandler();
+
+    await request(app).post('/user').send(creds);
+    jwt = await (await request(app).post('/user/token').send(creds)).body.token;
+  });
+
+  afterEach(afterEachHandler);
+
+  it('should get info about an existing user', async () => {
+    const res = await request(app)
+      .get('/user')
+      .auth(jwt, { type: 'bearer' })
+      .send();
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.username).toBe('tester');
+  });
+});
