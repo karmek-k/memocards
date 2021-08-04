0833ba3816a3201f4503ce85a29f1a503f84c755
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-04 17:29:46 +0200

    Remove an unneccessary try-catch

--- server/src/routes/deck.ts
+++ server/src/routes/deck.ts
@@ -7,14 +7,9 @@ import { User } from '../models/User';
 const router = Router();
 
 router.get('/:id', async (req, res) => {
-  const repo = getRepository(Deck);
-  let deck;
-
-  try {
-    deck = await repo.findOne(req.params.id, { relations: ['author'] });
-  } catch (e) {
-    return res.status(400).send();
-  }
+  let deck = await getRepository(Deck).findOne(req.params.id, {
+    relations: ['author']
+  });
 
   if (!deck) {
     return res.status(404).send();
