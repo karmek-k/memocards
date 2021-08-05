2ed709aaa79a91c04437f43448978522d365c4af
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-05 15:28:09 +0200

    Add morgan, cookie-parser and csurf middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,9 @@ import express from 'express';
 import passport from 'passport';
 import jwtStrategy from './config/jwtStrategy';
 import { Strategy as AnonymousStrategy } from 'passport-anonymous';
+import morgan from 'morgan';
+import csurf from 'csurf';
+import cookieParser from 'cookie-parser';
 import UserRouter from './routes/user';
 import DeckRouter from './routes/deck';
 import CardRouter from './routes/card';
@@ -15,9 +18,26 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 passport.use(new AnonymousStrategy());
 passport.use(jwtStrategy);
 app.use(passport.authenticate(['jwt', 'anonymous'], { session: false }));
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true
+      }
+    })
+  );
+
+  app.get('/csrf-token', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.send();
+  });
+}
 
 // Routes
 app.use('/user', UserRouter);
