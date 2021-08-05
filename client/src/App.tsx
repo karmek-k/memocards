f5125f076f72c6629d0e337cd7dea426190c7fff
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-05 19:39:43 +0200

    Create index and 404 routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  return <h1>Memocards</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={IndexPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/IndexPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return <h1>memocards</h1>;
+};
+
+export default IndexPage;
