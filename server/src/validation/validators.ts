5fe3e43b63068525ddc5472e6904fe7d38dbabfb
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-05 14:59:49 +0200

    Add card validation rules

--- server/src/validation/validators.ts
+++ server/src/validation/validators.ts
@@ -9,3 +9,9 @@ export const deckValidator = [
   body('name').isLength({ min: 2, max: 30 }),
   body('description').optional({ nullable: true }).isLength({ max: 500 })
 ];
+
+export const cardValidator = [
+  body('deckId').isNumeric(),
+  body('front').isLength({ max: 200 }),
+  body('back').isLength({ max: 200 })
+];
