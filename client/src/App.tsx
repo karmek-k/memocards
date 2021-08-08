c77f4d979c428c40663c9674ec890193f39df174
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-08 13:40:54 +0200

    Export the theme to the styles folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import NotFoundPage from './pages/NotFoundPage';
-import './global.css';
+import './styles/global.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './styles/theme';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={IndexPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
