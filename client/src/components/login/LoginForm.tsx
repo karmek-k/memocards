1fd3ad3db79d91b85eafa7737f3baafdb6339692
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-08 16:39:34 +0200

    Make the login form stateful

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -6,6 +6,7 @@ import {
   Button
 } from '@material-ui/core';
 import React from 'react';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
   paper: {
@@ -27,14 +28,32 @@ const useStyles = makeStyles({
 const LoginForm: React.FC = () => {
   const classes = useStyles();
 
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <Paper className={classes.paper}>
       <Typography variant="h2" className={classes.formHeader}>
         Log in
       </Typography>
-      <form className={classes.form} onSubmit={console.log}>
-        <TextField name="username" label="Username" />
-        <TextField name="password" type="password" label="Password" />
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          name="username"
+          label="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <TextField
+          name="password"
+          type="password"
+          label="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
         <Button
           variant="contained"
           color="primary"
