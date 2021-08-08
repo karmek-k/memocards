d664c98bbc72ebdf89e5601c21025f62cede106a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-08 13:22:06 +0200

    Set margin and padding of body to zero

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import NotFoundPage from './pages/NotFoundPage';
+import './global.css';
 
 function App() {
   return (
