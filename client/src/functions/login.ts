af42fa4d3d5bed715110a931c62b8b31d0d90019
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-09 16:11:30 +0200

    Create login function

---
+++ client/src/functions/login.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { LoginInputs } from '../components/login/LoginForm';
+
+interface TokenResponse {
+  token: string;
+}
+
+const login = (inputs: LoginInputs): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    axios
+      .post<TokenResponse>('/user/token', {
+        username: inputs.username,
+        password: inputs.password
+      })
+      .then(res => resolve(res.data.token))
+      .catch(() => reject());
+  });
+};
+
+export default login;
