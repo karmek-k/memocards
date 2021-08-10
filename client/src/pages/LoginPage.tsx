6adcef597a0e1bc57fdf76a056d5508d3a2c1b12
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-10 16:54:38 +0200

    Extract logging in logic to a hook

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,33 +1,17 @@
 import { Container } from '@material-ui/core';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useState } from 'react';
 import LoginForm, { LoginInputs } from '../components/login/LoginForm';
 import Layout from '../components/shared/Layout';
-import login from '../functions/login';
-import { useAppDispatch } from '../hooks/redux';
-import userSlice from '../redux/slices/userSlice';
+import useLogin from '../hooks/useLogin';
 
 const LoginPage: React.FC = () => {
   const [inputs, setInputs] = useState<LoginInputs>({
     username: '',
     password: ''
   });
-  const [loggingIn, setLoggingIn] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
-  const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (!inputs.username || !inputs.password) {
-      return;
-    }
-
-    setLoggingIn(true);
-    setError(false);
-    login(inputs)
-      .then(jwt => dispatch(userSlice.login(jwt)))
-      .catch(() => setError(true))
-      .finally(() => setLoggingIn(false));
-  }, [inputs, dispatch]);
+  const { loggingIn, error } = useLogin(inputs);
 
   return (
     <Layout>
