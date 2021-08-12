2e95970e0306044f2ba9804960dcd6d826db809b
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-12 18:40:58 +0200

    Fetch the deck for the overview page

--- client/src/pages/DeckOverviewPage.tsx
+++ client/src/pages/DeckOverviewPage.tsx
@@ -1,6 +1,8 @@
+import { CircularProgress } from '@material-ui/core';
 import React from 'react';
 import { useParams } from 'react-router';
 import Layout from '../components/shared/Layout';
+import useDeck from '../hooks/resources/useDeck';
 
 interface Params {
   deckId: string;
@@ -8,8 +10,21 @@ interface Params {
 
 const DeckOverviewPage: React.FC = () => {
   const { deckId } = useParams<Params>();
+  const deck = useDeck(Number.parseInt(deckId));
 
-  return <Layout></Layout>;
+  if (!deck) {
+    return (
+      <Layout>
+        <CircularProgress />
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <p>{Object.keys(deck)}</p>
+    </Layout>
+  );
 };
 
 export default DeckOverviewPage;
