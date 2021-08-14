f4f5b5ecdc40eaa39f6dfce5389a10ba093c6294
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-14 16:36:31 +0200

    Use the useRegister hook

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -7,6 +7,7 @@ import RegisterForm, {
 } from '../components/register/RegisterForm';
 import Layout from '../components/shared/Layout';
 import useLoggedIn from '../hooks/useLoggedIn';
+import useRegister from '../hooks/useRegister';
 
 const RegisterPage: React.FC = () => {
   const [inputs, setInputs] = useState<RegisterInputs>({
@@ -15,6 +16,11 @@ const RegisterPage: React.FC = () => {
   });
 
   const loggedIn = useLoggedIn();
+  const { done, error, registering } = useRegister(inputs);
+
+  if (done) {
+    return <p>Registered successfully</p>;
+  }
 
   if (loggedIn) {
     return <Redirect to="/dashboard" />;
@@ -23,7 +29,11 @@ const RegisterPage: React.FC = () => {
   return (
     <Layout>
       <Container maxWidth="sm">
-        <RegisterForm setInputsCallback={setInputs} />
+        <RegisterForm
+          setInputsCallback={setInputs}
+          error={error}
+          registering={registering}
+        />
       </Container>
     </Layout>
   );
