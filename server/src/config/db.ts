43d2a830347d6d41c2191724444f58b3b6d7d159
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-15 18:21:43 +0200

    Fix db.ts not loading data from the .env file

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,6 +4,8 @@ import { Card } from '../models/Card';
 import { Deck } from '../models/Deck';
 import { Review } from '../models/Review';
 import { User } from '../models/User';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config: ConnectionOptions = {
   type: 'postgres',
