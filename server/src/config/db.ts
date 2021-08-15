5903a38df39b4cda95ac285598df21736c6ff568
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-15 17:45:18 +0200

    Add Review and Answer cards to entities in db conf

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,12 +1,14 @@
 import { ConnectionOptions } from 'typeorm';
+import { Answer } from '../models/Answer';
 import { Card } from '../models/Card';
 import { Deck } from '../models/Deck';
+import { Review } from '../models/Review';
 import { User } from '../models/User';
 
 const config: ConnectionOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [User, Deck, Card],
+  entities: [User, Deck, Card, Review, Answer],
   synchronize: true
 };
 
