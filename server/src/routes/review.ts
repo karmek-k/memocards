80b8f4909416b8caa86faf6178ba23947ec039ff
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-15 16:25:04 +0200

    Move random utility functions

--- server/src/routes/review.ts
+++ server/src/routes/review.ts
@@ -2,34 +2,9 @@ import { Router } from 'express';
 import auth from '../middleware/auth';
 import fetchDeck from '../middleware/fetchDeck';
 import { Deck } from '../models/Deck';
+import { randomSubset } from '../utils/random';
 
 const router = Router();
-
-function randomItem(items: any[]) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
-function randomSubset<T>(items: T[], n: number) {
-  if (n >= items.length) {
-    return items;
-  } else if (n <= 0) {
-    return [];
-  }
-
-  const subset: T[] = [];
-
-  for (let i = 0; i < n; ++i) {
-    const remaining = items.filter(item => !subset.includes(item));
-
-    if (remaining.length === 0) {
-      break;
-    }
-
-    subset.push(randomItem(remaining));
-  }
-
-  return subset;
-}
 
 router.get('/:deckId', auth, fetchDeck('cards'), async (req, res) => {
   const deck = res.locals.deck as Deck;
