4ce6016f7a1fbe4ae841beb7e1c2048b4b1d0cf5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-15 13:09:15 +0200

    Create fetchDeck middleware

---
+++ server/src/middleware/fetchDeck.ts
@@ -0,0 +1,28 @@
+import { NextFunction } from 'express';
+import { getRepository } from 'typeorm';
+import { Deck } from '../models/Deck';
+import { User } from '../models/User';
+
+async function fetchDeck(req: any, res: any, next: NextFunction) {
+  let deck = await getRepository(Deck).findOne(req.params.id, {
+    relations: ['author']
+  });
+
+  if (!deck) {
+    return res.status(404).send();
+  }
+
+  let notYours = true;
+  if (req.user) {
+    notYours = (req.user as User).id !== deck.author.id;
+  }
+
+  if (deck.private && notYours) {
+    return res.status(403).send();
+  }
+
+  res.locals.deck = deck;
+  next();
+}
+
+export default fetchDeck;
--- server/src/routes/deck.ts
+++ server/src/routes/deck.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
 import auth from '../middleware/auth';
+import fetchDeck from '../middleware/fetchDeck';
 import { Deck } from '../models/Deck';
 import { User } from '../models/User';
 import validate from '../validation/middleware';
@@ -8,25 +9,8 @@ import { deckValidator } from '../validation/validators';
 
 const router = Router();
 
-router.get('/:id', async (req, res) => {
-  let deck = await getRepository(Deck).findOne(req.params.id, {
-    relations: ['author']
-  });
-
-  if (!deck) {
-    return res.status(404).send();
-  }
-
-  let notYours = true;
-  if (req.user) {
-    notYours = (req.user as User).id !== deck.author.id;
-  }
-
-  if (deck.private && notYours) {
-    return res.status(403).send();
-  }
-
-  return res.send(deck);
+router.get('/:deckId', fetchDeck, async (req, res) => {
+  return res.send(res.locals.deck);
 });
 
 router.get('/', auth, async (req, res) => {
