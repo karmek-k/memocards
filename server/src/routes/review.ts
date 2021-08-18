4cb2906ce2feca72ece93b02cbf216199dc17ecd
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-18 11:39:51 +0200

    Create GET /review, add user object to reviews

--- server/src/routes/review.ts
+++ server/src/routes/review.ts
@@ -6,6 +6,7 @@ import { Answer } from '../models/Answer';
 import { Card } from '../models/Card';
 import { Deck } from '../models/Deck';
 import { Review } from '../models/Review';
+import { User } from '../models/User';
 
 const router = Router();
 
@@ -13,6 +14,9 @@ router.post('/', auth, fetchDeck('query'), async (req, res) => {
   const deck = res.locals.deck as Deck;
   const cardRepo = getRepository(Card);
   const { answers } = req.body;
+
+  const { id: userId } = req.user! as User;
+  const user = (await getRepository(User).findOne(userId)) as User;
 
   if (!answers || answers.length === 0) {
     return res.status(400).send();
@@ -37,6 +41,7 @@ router.post('/', auth, fetchDeck('query'), async (req, res) => {
 
   const review = new Review();
   review.deck = deck;
+  review.user = user;
   review.answers = answers.map((a: Answer) => {
     const answer = new Answer();
     answer.correct = a.correct;
@@ -55,6 +60,16 @@ router.post('/', auth, fetchDeck('query'), async (req, res) => {
       incorrect: review.answers.filter(a => !a.correct).length
     }
   });
+});
+
+router.get('/', auth, async (req, res) => {
+  const { id } = req.user! as User;
+
+  const user = await getRepository(User).findOne(id, {
+    relations: ['reviews']
+  });
+
+  return res.send({ reviews: user?.reviews ?? [] });
 });
 
 export default router;
